7e7a7c6aae7514f933a1e805d05512fcabdd933a
Alex Joseph <alexandra.k.joseph2323@gmail.com>
2025-03-26 12:06:52 -0500

    unicode updates

--- unicode.js
+++ unicode.js
@@ -86,16 +86,18 @@ Assign the result to a variable named swappedString.
 //Starter Code
 // Task 1
 let inputString1 = "Code";
-let firstCodePoint; // Your code here
-let thirdCodePoint; // Your code here
+let firstCodePoint = inputString1.charCodeAt(0); // C
+let thirdCodePoint = inputString1.charCodeAt(2); // d
 
 // Task 2
-let wordFromCodePoints; // Your code here
+let wordFromCodePoints = String.fromCharCode(72, 101, 108, 108);//Hell
 
 // Task 3
 let inputString2 = "Launch";
-let swappedString; // Your code here
-
+let firstCharCode = inputString2.charCodeAt(0); //L
+let lastCharCode = inputString2.charCodeAt(inputString2.length - 1); //h
+let swappedString = 
+  String.fromCharCode(lastCharCode) + inputString2.slice(1, -1) + String.fromCharCode(firstCharCode);
 // Log all results
 console.log({
   firstCodePoint,
@@ -103,3 +105,7 @@ console.log({
   wordFromCodePoints,
   swappedString,
 });
+// 67
+// 100
+// Hell
+// hauncL
